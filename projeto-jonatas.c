132eb15d258130cc095fb494e0f77ac23182aeb6
Luiz Fernando <166060326+luizjulans@users.noreply.github.com>
2024-04-26 20:42:36 -0300

    Add files via upload

---
+++ projeto-jonatas.c
@@ -0,0 +1,243 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+	setlocale(LC_ALL,"portuguese");
+	int a, b, cr, v1,v2,v3,v4,v5,v6,v7,v8;
+	int valor,raro,contador,final, jonas; 
+	
+	printf("O animal a seguir representará você\n");
+	printf("Digite um número de 0 a 10 para descobrir um animal:\n");
+	scanf("%d",&a);
+
+	
+	switch (a){
+		case 1:
+			printf("Você achou um Leão!\n");
+			break;
+		
+		case 2:
+			printf("Você achou um Cachorro!\n");
+			break;
+		
+		case 3:
+			printf("Você achou um Macaco!\n");
+			break;
+		
+		case 4:
+			printf("Você achou uma Girafa!\n");
+			break;
+		
+		case 5:
+			printf("Você achou um saxofone, parabéns! (easteregg)\n");
+			break;
+		
+		case 6:
+			printf("Você achou um Tamanduá!");
+			break;
+		
+		case 7:
+			printf("Você achou um Elefante!");
+			break;
+		
+		case 8:
+			printf("Você achou uma Abelha!\n");
+			break;
+		
+		default: 
+			printf("Você não achou nada!\n");
+			break;
+		
+	}
+	
+		
+	printf("Escolha um número para uma surpresa\n (O número 7 pode ser interessante!!! Arrisque se quiser):");
+	scanf("%d",&cr); 	
+
+	
+ 	if (cr == 7){
+ 		printf("SIIIIU\n Você encontrou uma espécie rara!\n");
+ 		printf("O GOAT! O homem, A máquina, uma besta enjaulada, O MILIOR!!!\n");
+		printf("Você adquiriu o final secreto, PARABÉNS!\n");
+		 
+	 }
+	 
+	 else {
+		 
+
+printf("leve em consideração apenas as 2 primeiras linhas da matriz, a seguir:\n");
+printf("digite um número de um animal de 1 a 6\n");
+scanf("%i",&valor);
+
+	if (valor == 1) {
+
+
+	for (v1=67;v1<=68;v1++) {
+		
+		for (v2=65;v2<=66;v2++) {
+			
+			for (v3=67;v3<=68;v3++){
+				
+				for (v4=72;v4<=73;v4++) {
+					
+					for (v5=79;v5<=80;v5++) {
+						
+						for (v6=82;v6<=83;v6++) {
+							
+								for  (v7=82;v7<=83;v7++) {
+								
+										for (v8=79;v8<80;v8++) {
+											printf ("%c %c %c %c %c %c %c %c\n",v1,v2,v3,v4,v5,v6,v7,v8);
+											printf ("você é tão lindo quanto um cachorro, nhenhenhe!!!\n");
+											
+										
+}}}}}}}}}
+	
+	else if (valor == 2) {
+		
+			for (v1=84;v1<=85;v1++) {
+		
+		for (v2=73;v2<=74;v2++) {
+			
+			for (v3=71;v3<=72;v3++){
+				
+				for (v4=82;v4<=83;v4++) {
+					
+					for (v5=69;v5<=70;v5++) {
+						
+
+		printf ("%c %c %c %c %c \n",v1,v2,v3,v4,v5);
+		printf ("você é um homem feroz e encontrou um tigre!!\n");
+											
+										
+										
+}}}}}}	
+	 else if   (valor == 3) {
+		
+		
+		for (v1=69;v1<=70;v1++) {
+		
+		for (v2=76;v2<=77;v2++) {
+			
+			for (v3=69;v3<=70;v3++){
+				
+				for (v4=70;v4<=71;v4++) {
+					
+					for (v5=65;v5<=66;v5++) {
+						
+						for (v6=78;v6<=79;v6++) {
+							
+								for  (v7=84;v7<=85;v7++) {
+								
+										for (v8=69;v8<70;v8++) {
+											printf ("%c %c %c %c %c %c %c %c\n",v1,v2,v3,v4,v5,v6,v7,v8);
+											printf ("Se comer demais,vai ficar igual a esse animal, enorme com uma grande tromba!!\n");
+											
+										
+}}}}}}}}}
+	
+	else if (valor == 4) {
+		
+		
+		printf("você encontrou um animal diferente... As matrizes não se formaram...\n Trata-se de um evento raro!!\n Digite um número entre 1 e 10\n");
+		scanf ("%i",&raro);
+		
+		
+		if (raro == 7) {
+			
+			
+			for (v1=68;v1<=79;v1++) {
+		
+		for (v2=73;v2<=74;v2++) {
+			
+			for (v3=79;v3<=80;v3++){
+				
+				for (v4=71;v4<=72;v4++) {
+					
+					for (v5=69;v5<=70;v5++) {
+						
+						for (v6=78;v6<=79;v6++) {
+							
+								for  (v7=69;v7<=70;v7++) {
+								
+										for (v8=83;v8<84;v8++) {
+											printf ("%c %c %c %c %c %c %c %c\n",v1,v2,v3,v4,v5,v6,v7,v8);
+											printf ("Isso é uma espécie não identificada, um professor Shiny!!!!\n");
+											
+										
+										
+}}}}}}}}}
+	else 
+		{
+			printf("a criatura que estava aqui fugiu, que pena ;(");
+		}		
+		
+		
+}
+
+	else if (valor == 5) {
+		
+		printf("essa não é uma tabela comum, prepare-se para a surpresa!!!\n");
+		printf("digite um número de 1 a 10 para aprender uma arte secreta \n");
+		scanf("%i",&valor);
+		
+		if (valor > 5) {
+		
+		for (contador = 1;contador <= 1;contador++) {
+			
+			printf("Parabéns, graças a sua resiliência você aprendeu a matemática do Dragão. Um tipo de matemática que apenas escolhidos conseguem aprender! \n");
+			printf("está pronto para aprender? Sim ou Não?\n Digite 1 para Sim e 2 para Não  \n");
+			scanf("%i",&final);
+			
+			
+			if (final == 1) {
+				
+				printf("Parabéns, você agora é um aprendiz do Dragão Vidyadhara Dan Heng e ganhou o direito de treinar com ele \n");
+				printf("você é o segundo mestre dragão vivo\n");
+				printf ("Você terminou o jogo ;)\n");
+				printf("final 1 de 3");
+			}
+			
+			else {
+				
+				printf("perdedor, não é digno de reinvidicar a recompensa final\n");
+				
+			}
+		
+			
+		}
+		
+						
+		}
+		
+		else {
+			
+			printf("Você ganhou acesso ao computador de diogenes, parabéns, isso significa que você adquiriu um FINAL RUIM!\n");
+		}	
+	
+		}
+		
+		else if(valor == 6) {
+			printf("Escolha um número de 1 a 10, Querido Professor Jonatas:\n");
+			scanf("%d",&jonas);
+			
+				if (jonas == 6) {
+					printf("Você é divino como o Thor,\nForte como o Hulk \ne rico como o homem de ferro");
+				}
+				
+		else if(jonas == 10){
+			printf("Essa é a nota que nós queremos :) = 10");
+		}
+		
+				else {
+					printf("Você não é tribunal vivo mas eu quero ser julgado por você");
+				}
+
+		}
+			
+	 }
+	
+	
+	
+	return 0;
+}
